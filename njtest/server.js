4f18e64db9c40dea8e78441d7e5dc12eefa0d8ba
Dylan Lokatys <dlokatys@hotmail.com>
2022-03-31 21:24:19 -0400

    first commit from Dylan

--- njtest/server.js
+++ njtest/server.js
@@ -7,4 +7,4 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}).listen(3000);
