68227f9d48b7ba676431a1cb1c838cc10db7a130
Renato Dodani <renato.dodani1@gmail.com>
2022-04-01 20:36:18 -0400

    Added login and signup components

---
+++ client/src/components/loginForm/loginForm.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './loginForm.css';
+
+
+export function LoginForm() {
+
+return (
+  <div>
+  <div className="sidenav">
+         <div className="login-main-text">
+            <h2>MoodWeb</h2>
+            <p>Login or register from here to access.</p>
+         </div>
+      </div>
+      <div className="main">
+         <div className="col-md-6 col-sm-12">
+            <div className="login-form">
+               <form>
+                  <div className="form-group">
+                     <label>Email</label>
+                     <input type="text" className="form-control" placeholder="Email"></input>
+                  </div>
+                  <div className="form-group">
+                     <label>Password</label>
+                     <input type="password" className="form-control" placeholder="Password"></input>
+                  </div>
+                  <button type="submit" className="btn btn-black">Login</button>
+                  <button type="submit" className="btn btn-secondary">Register</button>
+               </form>
+            </div>
+         </div>
+      </div>
+      </div>
+)}
