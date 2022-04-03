5e104fc7256e1712cb63886cb3527cbede758395
Renato Dodani <renato.dodani1@gmail.com>
2022-04-03 18:06:31 -0400

    Added routes files for authentication

---
+++ routes/auth-routes/loginRoute.ts
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcrypt');
+// const config = require('config');
+
+// authentication routes
+router.post("/", async (req, res) => {
+	
+});
+
