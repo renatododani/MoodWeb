e932fed8b7725f7b3d4d873c25c02ddfda87f102
Renato Dodani <renato.dodani1@gmail.com>
2022-04-05 19:42:40 -0400

    Added HTML and css code for the homepage component

--- client/src/components/moodHome/moodHome.tsx
+++ client/src/components/moodHome/moodHome.tsx
@@ -1,14 +1,49 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import '../moodHome/moodHome.css'
 
 export function MoodHome() {
-  const [user, setUser] = useState("User");
-  return (
-    <div>
-      <div>
-        <h1>Hello {user}! </h1>
-        <Link to="/purple">Purple</Link>
-      </div>
+const [user, setUser] = useState("User");
+return (
+<div>
+  <div>
+    <h1>Hello {user}! </h1>
+    
+  </div>
+  <section>
+    <div className="bowl-row">
+    <div className="bowl" >
+      <div className="liquid"><Link to="/purple">Purple</Link></div>
     </div>
-  );
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    </div>
+
+
+    <div className="bowl-row">
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    </div>
+
+    
+    <div className="bowl-row">
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    <div className="bowl">
+      <div className="liquid"></div>
+    </div>
+    </div>
+
+  </section>
+</div>
+);
 }
