7778f7fb0c26139e258825704d92acf0ed05b2c9
Renato Dodani <renato.dodani1@gmail.com>
2022-04-04 22:12:40 -0400

    Added a nusic player component

---
+++ client/src/components/musicplayer/musicplayer.tsx
@@ -0,0 +1,25 @@
+export function MusicPlayer() {
+    return (
+        <div> 
+        {/* <div className="audio-player">
+          <div id="play-btn"></div>
+          <div className="audio-wrapper" id="player-container" href="javascript:;">
+            <audio id="player" ontimeupdate="initProgressBar()">
+                    <source src="http://www.lukeduncan.co/oslo.mp3" type="audio/mp3">
+                  </audio>
+          </div>
+          <div className="player-controls scrubber">
+            <p>Oslo <small>by</small> Holy Esque</p>
+            <span id="seekObjContainer">
+                    <progress id="seekObj" value="0" max="1"></progress>
+                  </span>
+            <br>
+            <small style="float: left; position: relative; left: 15px;" className="start-time"></small>
+            <small style="float: right; position: relative; right: 20px;" className="end-time"></small>
+      
+          </div>
+          <div className="album-image" style="background-image: url('https://artwork-cdn.7static.com/static/img/sleeveart/00/051/614/0005161476_350.jpg')"></div>
+        </div> */}
+        </div>
+    )
+}
