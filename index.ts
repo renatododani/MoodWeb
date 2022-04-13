40f2ee74fd1fed569694b796783c775281d5dbdb
Teri Klosky <t.klosky@yahoo.com>
2022-04-13 18:33:33 -0400

    index.ts

--- index.ts
+++ index.ts
@@ -1,26 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import pg from 'pg-promise';
-import { signupRoutes } from './routes/auth-routes/signupRoute';
-import { loginRoute } from './routes/auth-routes/loginRoute';
+import express from "express";
+import cors from "cors";
+import pg from "pg-promise";
+import { signupRoutes } from "./routes/auth-routes/signupRoute";
+import { loginRoute } from "./routes/auth-routes/loginRoute";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/', signupRoutes);
-app.use('/', loginRoute);
+app.use("/", signupRoutes);
+app.use("/", loginRoute);
 
 const port = 3050;
 export const db = pg()({
-
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'mlmlml',
-    database: 'MoodWeb'
+  host: "localhost",
+  port: 5433,
+  user: "postgres",
+  password: "MapleT10",
+  database: "moodwebDb",
 });
-
-
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
