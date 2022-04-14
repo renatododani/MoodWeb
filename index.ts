b7fffbd87929f86336c0bc296f0b5e85bb9873ef
Teri Klosky <t.klosky@yahoo.com>
2022-04-13 22:07:22 -0400

    update:

--- index.ts
+++ index.ts
@@ -1,71 +1,26 @@
-<<<<<<< HEAD
 import express from "express";
 import cors from "cors";
 import pg from "pg-promise";
 import { signupRoutes } from "./routes/auth-routes/signupRoute";
 import { loginRoute } from "./routes/auth-routes/loginRoute";
+import { purpleRoute } from "./routes/purpleRoutes";
 require("dotenv").config();
-=======
-import express from 'express';
-import cors from 'cors';
-import pg from 'pg-promise';
-import { signupRoutes } from './routes/auth-routes/signupRoute';
-import { loginRoute } from './routes/auth-routes/loginRoute';
-import { purpleRoute } from './routes/purpleRoutes';
-import { redRoute } from './routes/redRoutes';
-import { orangeRoute } from './routes/orangeRoutes';
-import { yellowRoute } from './routes/yellowRoutes';
-import { greenRoute } from './routes/greenRoutes';
-import { blackRoute } from './routes/blackRoutes';
-import { blueRoute } from './routes/blueRoutes';
-require('dotenv').config();
->>>>>>> b225041d49b8fd97ca04c046e8bbb15299fc47db
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
 app.use("/", signupRoutes);
 app.use("/", loginRoute);
-=======
-app.use('/', signupRoutes);
-app.use('/', loginRoute);
-<<<<<<< HEAD
-app.use('/', purpleRoute)
->>>>>>> b225041d49b8fd97ca04c046e8bbb15299fc47db
-=======
-app.use('/', purpleRoute);
-app.use('/', redRoute);
-app.use('/', orangeRoute);
-app.use('/', yellowRoute);
-app.use('/', greenRoute);
-app.use('/', blackRoute);
-app.use('/', blueRoute);
->>>>>>> 468cee45479812186cf1a4a4db6732a7369a8f34
-
+app.use("/", purpleRoute);
 const port = 3050;
 
 export const db = pg()({
-<<<<<<< HEAD
   host: "localhost",
   port: 5432,
   user: "postgres",
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-=======
-
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-<<<<<<< HEAD
-    password: '2cshbkukdd',
-    database: 'moodwebDB'
->>>>>>> b225041d49b8fd97ca04c046e8bbb15299fc47db
-=======
-    password: 'mlmlml',
-    database: 'MoodWeb'
->>>>>>> 468cee45479812186cf1a4a4db6732a7369a8f34
+  password: "2cshbkukdd",
+  database: "moodwebDB",
 });
 
 app.listen(port, () =>
