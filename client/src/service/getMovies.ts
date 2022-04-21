f805fae88091e299da9e52bf585f64d2b4cccb71
Dylan <dlokatys@hotmail.com>
2022-04-20 21:27:49 -0400

    all services completed

--- client/src/service/getMovies.ts
+++ client/src/service/getMovies.ts
@@ -9,30 +9,30 @@ export interface Movie {
 }
 
 export function getPurpleMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/purple-movies')
     .then(response => response.data)
 }
 export function getRedMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/red-movies')
     .then(response => response.data)
 }
 export function getOrangeMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/orange-movies')
     .then(response => response.data)
 }
 export function getYellowMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/yellow-movies')
     .then(response => response.data)
 }
 export function getGreenMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/green-movies')
     .then(response => response.data)
 }
 export function getBlueMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/blue-movies')
     .then(response => response.data)
 }
 export function getBlackMovies(){
-    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Movie[]>('https://mood-web-backend.vercel.app/black-movies')
     .then(response => response.data)
 }
--- client/src/service/getQuotes.ts
+++ client/src/service/getQuotes.ts
@@ -9,36 +9,36 @@ export interface Quote {
 
 //Purple component
 export function getPurpleQuote() {
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/purple-quotes')
     .then(response => response.data)
 }
 export function getRedQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/red-quotes')
     .then(response => response.data)
 }
 export function getOrangeQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/orange-quotes')
     .then(response => response.data)
 }
 export function getYellowQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/yellow-quotes')
     .then(response => response.data)
 }
 export function getGreenQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/green-quotes')
     .then(response => response.data)
 }
 export function getBlueQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/blue-quotes')
     .then(response => response.data)
 }
 export function getBlackQuote() {
 
-    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/')
+    return axios.get<Quote|undefined>('https://mood-web-backend.vercel.app/black-quotes')
     .then(response => response.data)
 }
