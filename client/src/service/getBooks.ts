8d050063223e946b3f536f84e18f2921ef3db620
Dylan <dlokatys@hotmail.com>
2022-04-20 21:24:29 -0400

    fixed book service file

--- client/src/service/getBooks.ts
+++ client/src/service/getBooks.ts
@@ -10,30 +10,30 @@ export interface Book{
 }
 
 export function getPurpleBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/purple-books')
     .then(response => response.data)
 }
 export function getRedBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/red-books')
     .then(response => response.data)
 }
 export function getOrangeBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/orange-books')
     .then(response => response.data)
 }
 export function getYellowBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/yellow-books')
     .then(response => response.data)
 }
 export function getGreenBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/green-books')
     .then(response => response.data)
 }
 export function getBlueBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/blue-books')
     .then(response => response.data)
 }
 export function getBlackBooks(){
-    return axios.get<Book[]>('https://mood-web-backend.vercel.app/')
+    return axios.get<Book[]>('https://mood-web-backend.vercel.app/black-books')
     .then(response => response.data)
 }
